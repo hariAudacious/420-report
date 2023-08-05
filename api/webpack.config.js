37e325bbaa7aea7717e66f802b37cdd6f7f55b15
hariAudacious <hariom.sharma@techiegent.com>
2023-08-06 00:55:01 +0530

    feat:added build logic

---
+++ api/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "[name].js",
+    clean: true,
+  },
+  mode: "development",
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+};
